26ff901b0303495e410c2fbcd7756cb9f5123ece
Kevin <kevin.nazgul@gmail.com>
2016-08-28 22:57:49 +0800

    Revert "改为使用experimental版本编译jni"
    This reverts commit 0f5d82e2d81267b8a9c3334218e86f874e99189b.

--- app/src/main/java/com/winomtech/sdk/utils/SdkConstants.java
+++
@@ -1,9 +0,0 @@
-package com.winomtech.sdk.utils;
-
-/**
- * @since 2015-03-07
- * @author kevinhuang 
- */
-public class SdkConstants {
-	public final static boolean	LOG_TO_LOGCAT	= true;
-}
---
+++ sdk/src/com/winomtech/androidmisc/sdk/thread/ThreadTask.java
@@ -0,0 +1,16 @@
+package com.winomtech.androidmisc.sdk.thread;
+
+/**
+ * @since 2015-03-10
+ * @author kevinhuang 
+ */
+public class ThreadTask implements Runnable {
+	public int prority;
+
+	public Runnable task;
+
+	@Override
+	public void run() {
+		
+	}
+}
